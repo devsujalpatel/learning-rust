347e501a4594b7c222cb0de1ab4f710c6fd79d08
sujalpatelcoder <sujal7455@gmail.com>
2025-07-21 15:30:05 +0530

    numbers in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+
+    let x:i32 = -5; // number that can be both +ve and -ve
+    let y:u32 = 1000; // number that can only be +ve
+    let z:f32 = 33.001;  // number that can be both +ve and -ve but has decimal places
+
+    print!("x: {}, y: {}, z: {}", x, y, z)
+
 }
