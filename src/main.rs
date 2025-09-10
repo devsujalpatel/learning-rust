2145013db15aaa35bfee6a5f7f5313c7016cb69b
devsujalpatel <sujal7455@gmail.com>
2025-09-11 00:11:46 +0530

    for loop and fucntion in rust

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
 fn main() {
-    let str = String::from("Hello");
-    println!("{}, {}", str, "Rust!");
+ 
+ for i in 1..11 {
+    println!("{} ", i);
+ }
+
+ let sentence: String = String::from("My name is sujal");
+
+ let first_word = get_first_word(sentence);
+
+ println!("First word is: {}", first_word)
+
 }
+
+ fn get_first_word(sentence: String) -> String {
+     let mut ans = String::from("");
+     for char in sentence.chars() {
+        ans.push_str(char.to_string().as_str());
+        if char == ' ' {
+            break;
+        }
+     }
+     return  ans;
+ }
